31987f8e0e9b7e739df5405a5f3a1c3ec4acb423
erbrchoudhary <erbrchoudhary0789@gmail.com>
2022-03-09 19:03:42 +0530

    initial commit

--- While_loop_problems.py
+++ While_loop_problems.py
@@ -52,43 +52,140 @@
 
 #sum of number in list
 
-list2=[]
-s=0
-while s<100:
-    s += 1
-    list2.append(s)
+# list2=[]
+# s=0
+# while s<100:
+#     s += 1
+#     list2.append(s)
+#
+# print(list2)
+#
+# i=0
+# sum=0
+# while i <len(list2):
+#     sum += list2[i]
+#     i+=1
+# print(sum)
+#
+# # sum of factriol of 1 to 20
+# def fact(n):
+#     if n==1:
+#         return n
+#     else:
+#         return n*(fact(n-1))
+# list3=[]
+# g=1
+# h=fact(g)
+# while g<20:
+#     g+=1
+#     list3.append(fact(g))
+# print(list3)
+# i=0
+# sum=0
+# while i<len(list3):
+#     sum=sum+list3[i]
+#     i+=1
+# print(sum)
+#
+# # Poping out item from list
+# fruitsList = ["Mango","Apple","Orange","Guava"]
+# while fruitsList:
+#     print(fruitsList.pop()) #The pop() will be accepting only one argument for its execution
+# print(fruitsList)
+#
+# #Printing all later except
+#
+# i=0
+# word = "Hello"
+# #print except e and o
+# while i<len(word):
+#     if word[i]=='e' or word[i]=='o':
+#         i+=1
+#         continue
+#     print("Returned latter :  " ,word[i])
+#     i+=1
 
-print(list2)
+# Taking input
 
-i=0
-sum=0
-while i <len(list2):
-    sum += list2[i]
-    i+=1
-print(sum)
+# n=int(input("Enter a number : "))
+#
+# while n!=0:
+#
+#     n= int(input("Enter 0 to quit "))
 
-# sum of factriol of 1 to 20
-def fact(n):
-    if n==1:
-        return n
-    else:
-        return n*(fact(n-1))
-list3=[]
-g=1
-h=fact(g)
-while g<20:
-    g+=1
-    list3.append(fact(g))
-print(list3)
-i=0
-sum=0
-while i<len(list3):
-    sum=sum+list3[i]
-    i+=1
-print(sum)
+# Convert decimal to binary
+# num= int(input("Enter a number : "))
+# b=0
+# p=1
+# n=num
+# while n>0:
+#     rem=n%2  # give reminder
+#     b+= rem*p
+#     p=p*10
+#     n=n//2  # floor division
+# print("Binary Value : ",b)
+# print(1%2)
 
-# Poping out item from list
-fruitsList = ["Mango","Apple","Orange","Guava"]
-while fruitsList:
-    print(fruitsList.pop()) #The pop() will be accepting only one argument for its execution
-print(fruitsList)
+#Average of 5 number
+# p = 5
+# sum = 0
+# count = 0
+#
+# while p > 0:
+#     count += 1
+#     f = int(input("Enter the number :  "))
+#     sum += f
+#     p -= 1
+
+# average = sum / count
+# print("Average of given Numbers:", average)
+
+# printing the squre
+# n = 1
+#
+# while n <= 5:
+#     squareNum = n**2 #squre of number
+#     print(n,squareNum)
+#     n += 1
+#
+# # Finding the number of multiple
+# n = int(input("Enter an integer: "))
+#
+# i = 1
+# while i <= 10:
+#     mul = i*n
+#     i += 1
+#     print(mul)
+
+#reversing the number
+# n = int(input("Enter a number: "))
+# rev = 0
+#
+# while n != 0:
+#     r = n % 10
+#     rev = rev * 10 + r
+#     n = n // 10
+#
+# print("Reversed number:", rev)
+
+# sum of even numbers
+i = 0
+sum = 0
+n = int(input("Enter the number n: "))
+
+while i <= n:
+    if i % 2 == 0:
+        sum += i
+    i += 1
+
+print("Sum of even numbers till n:", sum)
+
+# Finding the factroil
+n = int(input("Enter the number: "))
+f =n
+r = 1
+
+while f != 0 :
+    r *= f
+    f -= 1
+print("Factorial of",n,"is:",r)
